053ff3092aa9ce91eda1dbd7243986334b3651c8
root <root@localhost.localdomain>
2018-03-14 11:42:29 +0330

    add start iac page

--- main/urls.py
+++ main/urls.py
@@ -7,10 +7,11 @@ app_name = 'main'
 urlpatterns = [
     # path('index', TemplateView.as_view(template_name='main/index.html')),
     path('update', TemplateView.as_view(template_name='main/start.html')),
+    path('update_iac, TemplateView.as_view(template_name='main/start2.html')),
     path('play', RedirectView.as_view(url='/update')),
-    path('play_iac', RedirectView.as_view(url='/update')),
-    path('table_iac', RedirectView.as_view(url='/update')),
+    path('play_iac', RedirectView.as_view(url='/update_iac')),
+    path('table_iac', RedirectView.as_view(url='/update_iac')),
     path('cup', RedirectView.as_view(url='/update')),
-    path('cup_iac', RedirectView.as_view(url='/update')),
+    path('cup_iac', RedirectView.as_view(url='/update_iac')),
 
 ]
