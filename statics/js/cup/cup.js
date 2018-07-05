9684f4f957ed71b078335f6ec6b9dca856a73c1b
Unknown <ebimosi14@gmail.com>
2018-07-05 14:41:58 +0430

    user password recovery
    update register and login template

--- statics/js/cup/cup.js
+++ statics/js/cup/cup.js
@@ -1,16 +1,16 @@
 function add_team() {
-    $.ajax({
-        url: '/add_team_ajax/',
-        data: {
-            'add': true
-        },
-        method: "POST",
-        dataType: 'json',
-        success: function (data) {
-            var add_btn = document.getElementById('add-team-btn');
-            add_btn.style.backgroundColor = '#5e5e5e';
-            add_btn.removeAttribute('onclick');
-            window.location.reload(false);
-        }
-    });
+  $.ajax({
+    url: '/add_team_ajax/',
+    data: {
+      'add': true
+    },
+    method: "POST",
+    dataType: 'json',
+    success: function(data) {
+      var add_btn = document.getElementById('add-team-btn');
+      add_btn.style.backgroundColor = '#5e5e5e';
+      add_btn.removeAttribute('onclick');
+      window.location.reload(false);
+    }
+  });
 }
