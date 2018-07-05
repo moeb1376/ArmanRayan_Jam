2d787de816ec382c353e110c35aec546c4c37797
Unknown <ebimosi14@gmail.com>
2018-07-05 14:40:49 +0430

    update main url
    delete migrations file and add online match new view
    
    Former-commit-id: 206ee33e8c72c3fbcf0137b650516ee47f302e07

--- main/urls.py
+++ main/urls.py
@@ -1,15 +1,13 @@
 from django.contrib.auth import views as auth_view
-from django.urls import path, include, re_path
+from django.urls import path
 from django.views.generic import TemplateView, RedirectView
 from .views import *
 
 app_name = 'main'
 urlpatterns = [
-    # path('index', TemplateView.as_view(template_name='main/index.html')),
     path('update', TemplateView.as_view(template_name='main/start.html')),
     path('update_iac', TemplateView.as_view(template_name='main/start2.html')),
     path('play_iac', RedirectView.as_view(url='/play')),
     path('table_iac', RedirectView.as_view(url='/table')),
     path('cup_iac', RedirectView.as_view(url='/cup')),
-    #serfan baraye chek kardane :D
 ]
