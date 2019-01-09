4212d004cb3336f14c494d6738f533cca7b9e591
moeb <ebimosi14@gmail.com>
2019-01-09 20:48:27 +0330

    add new spc main ui (not fixed)
    Former-commit-id: a31ae9c905f6e304a42175e381007f656a2482ea

--- SPC_main/urls.py
+++ SPC_main/urls.py
@@ -6,6 +6,7 @@ from .views import *
 app_name = 'SPC_main'
 urlpatterns = [
     path('jaam', SPC_main_page, name='SPC_main_page'),
+    path('new_jaam', new_SPC_main_page, name='new_SPC_main_page'),
     path('logout', team_logout, name='logout'),
     path('table', table_view, name='table-spc'),
     path('cup', cup_view, name='cup-view'),
