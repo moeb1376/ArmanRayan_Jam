6b495826d6e518a9861be923a7f911bdb44433b2
moeb <ebimosi14@gmail.com>
2019-01-24 17:03:09 +0330

    add friendly page to online match

--- online_match/urls.py
+++ online_match/urls.py
@@ -4,10 +4,12 @@ from django.views.generic import RedirectView
 
 from .views import *
 
-app_name = 'online_match'
 urlpatterns = [
     path('play', online_match, name='online_match'),
+    path('new_play', online_match, name='new_online_match'),
     path('log', log_view, name='log'),
+    path('new_log', log_view, name='new_log'),
     url(r'^play_online_ajax/$', play_online_ajax, name='play_online_ajax'),
     url(r'^file_upload_ajax/$', upload_view, name='file_upload_ajax'),
 ]
+app_name = 'online_match'
