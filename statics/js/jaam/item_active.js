d13e67087f366bc0f3946f2c54f062d2564f2f3a
moeb <ebimosi14@gmail.com>
2019-02-13 19:17:07 +0330

    add active icon to pages

--- statics/js/jaam/item_active.js
+++ statics/js/jaam/item_active.js
@@ -1,13 +1,21 @@
 $(document).ready(function () {
     let path = document.location.pathname;
-    if (path.includes("new_jaam")) {
+    if (path.match("jaam/$") ) {
         $("#team_detail_item").addClass("icon-active");
         $("#team_detail_mobile_item").addClass("icon-active");
-    } else if (path.includes("jaam_table")) {
+    } else if (path.match("jaam_tables/$")) {
         $("#table_jaam_item").addClass("icon-active");
         $("#table_jaam_mobile_item").addClass("icon-active");
-    } else if (path.includes("table")) {
+    } else if (path.match("table/$")) {
         $("#cup_item").addClass("icon-active");
         $("#cup_mobile_item").addClass("icon-active");
+    } else if (path.match("play/$")) {
+        $("#friendly_item").addClass("icon-active");
+        $("#friendly_mobile_item").addClass("icon-active");
+    } else if (path.match("log/$")) {
+        $("#log_item").addClass("icon-active");
+        $("#log_mobile_item").addClass("icon-active");
+    } else if (path.match("setting/$")) {
+        $("#setting_item").addClass("icon-active");
     }
 });
