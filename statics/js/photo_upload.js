5147389b58e0dfe434942efc0b6376d8b69663c9
moeb <ebimosi14@gmail.com>
2019-02-14 16:00:07 +0330

    fix upload code problem

--- statics/js/photo_upload.js
+++ statics/js/photo_upload.js
@@ -8,20 +8,22 @@ $(function () {
         dataType: 'json',
         sequentialUploads: true, /* 1. SEND THE FILES ONE BY ONE */
         start: function (e) {  /* 2. WHEN THE UPLOADING PROCESS STARTS, SHOW THE MODAL */
-            $("#modal-progress").modal("show");
+            // $("#modal-progress").modal("show");
+            console.log("start");
         },
         stop: function (e) {  /* 3. WHEN THE UPLOADING PROCESS FINALIZE, HIDE THE MODAL */
-            $("#modal-progress").modal("hide");
-            alert("بارگذاری با موفقیت انجام شد")
+            // $("#modal-progress").modal("hide");
+            console.log("بارگذاری با موفقیت انجام شد");
         },
         progressall: function (e, data) {  /* 4. UPDATE THE PROGRESS BAR */
             var progress = parseInt(data.loaded / data.total * 100, 10);
             var strProgress = progress + "%";
             // $(".progress-bar").css({"width": strProgress});
-            var progress_bar = $("#upload_progress_bar");
-            progress_bar.css({"width": strProgress});
+            // var progress_bar = $("#upload_progress_bar");
+            // progress_bar.css({"width": strProgress});
             // $(".progress-bar").text(strProgress);
-            progress_bar.text(strProgress);
+            // progress_bar.text(strProgress);
+            console.log(strProgress);
         },
         done: function (e, data) {
         }
