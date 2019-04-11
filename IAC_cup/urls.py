8cc098c95ccd4c35151914332bd07bec2432e906
javad14042 <j.agheleh@yahoo.com>
2019-04-11 16:51:13 +0430

    Add iac_cup.html for online test

--- IAC_cup/urls.py
+++ IAC_cup/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
+from .views import *
+
 from django.views.generic import RedirectView
 
 app_name = 'register'
 urlpatterns = [
+    path('iac_cup/',render_page,name='render_page')
 ]
