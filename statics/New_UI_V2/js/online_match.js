804eaf0a537b807f7c23854f95f5ef5891179f0a
moeb <ebimosi14@gmail.com>
2019-08-04 16:36:15 +0430

    change image src to global variable change progress bar color

--- statics/New_UI_V2/js/online_match.js
+++ statics/New_UI_V2/js/online_match.js
@@ -1,13 +1,11 @@
 $("select[name='second-team']").change(function () {
-    console.log("jquery salam")
     let id_selected = $(this).children("option:selected").val();
-    if (id_selected == 1){
-        $("img[name='team2-img']").attr("src","../img/unknown.jpg");
+    if (id_selected == 1) {
+        $("img[name='team2-img']").attr("src", unknownSrc);
         $("h4[name='team2-name']").text("تیم تصادفی");
         $("h5[name='team2-university']").text("");
-    }
-    else if (id_selected >=2){
-        $("img[name='team2-img']").attr("src","../img/Armankadeh_logo_300.png");
+    } else if (id_selected >= 2) {
+        $("img[name='team2-img']").attr("src", armankadehLogoSrc);
         $("h4[name='team2-name']").text("هوش آرمانکده");
         if (id_selected == 2)
             $("h5[name='team2-university']").text("آسان");
