a20da31f0ede246a942bf4ca9b31ce0cc8cb7d50
moeb <ebimosi14@gmail.com>
2019-08-15 16:25:23 +0430

    add constants file for choice field

---
+++ register/constants.py
@@ -0,0 +1,31 @@
+# <-- مقطع -->
+BACHELOR = 1
+MASTER = 2
+PHD = 3
+GRADE = ((BACHELOR, "کارشناسی"), (MASTER, "کارشناسی ارشد"), (PHD, "دکتری"))
+# <-- -->
+# <-- دانشگاه -->
+UT = 1
+KHU = 2
+IUST = 3
+SUT = 4
+AUT = 5
+SBU = 6
+KNTU = 7
+UNIVERSITY = ((UT, "تهران"), (KHU, "خوارزمی"), (IUST, "علم و صنعت"), (SUT, "صنعتی شریف"), (AUT, "صنعتی امیرکبیر"),
+              (SBU, "شهید بهشتی"), (KNTU, "خواجه نصری الدین طوسی"))
+# <-- -->
+# <-- زبان‌های برنامه نویسی -->
+CPP = 1
+JAVA = 2
+PYTHON = 3
+MATLAB = 4
+PROGRAMMING_LANGUAGES = ((CPP, "C++"), (JAVA, "Java"), (PYTHON, "Python3"), (MATLAB, "Matlab"))
+# <-- -->
+# <-- مسابقات -->
+SPC = 1
+IAC_TEXT = 3
+IAC_SOUND = 4
+IAC_PICTURE = 5
+COMPETITION = ((SPC, "SPC"), (IAC_TEXT, "IAC-پردازش متن"), (IAC_SOUND, "IAC-پردازش صوت"), (IAC_PICTURE, "IAC-پردازش تصویر"))
+# <-- -->
