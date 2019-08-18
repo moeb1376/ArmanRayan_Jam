c2fbc0a93349b968654cd4fe6a2bd24aac28d428
moeb <ebimosi14@gmail.com>
2019-08-18 17:19:15 +0430

    change jaam url to index and jaam_tables to cup-tables in item active

--- statics/js/jaam/item_active.js
+++ statics/js/jaam/item_active.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     let path = document.location.pathname;
     let mobile_item
-    console.log(path.match("jaam/$"));
-    if (path.match("jaam/$")) {
+    console.log(path.match("index/$"));
+    if (path.match("index/$")) {
         $("#team_detail_item").addClass("icon-active");
         mobile_item = $("#team_detail_mobile_item");
         mobile_item.addClass("icon-active");
         mobile_item.parent("a").addClass("sidenav_field_active");
-    } else if (path.match("jaam_tables/$")) {
+    } else if (path.match("cup_tables/$")) {
         $("#table_jaam_item").addClass("icon-active");
         mobile_item = $("#table_jaam_mobile_item");
         mobile_item.addClass("icon-active");
